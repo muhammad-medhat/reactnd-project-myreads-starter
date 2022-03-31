0b51a6e1a716a0dab878d1ba84e2fd42ead18945
Muhammad <Muhammad.medhat@gmail.com>
2022-03-31 19:16:36 +0200

    debounce search

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Search from './components/Search'
 import BooksContent from './components/BooksContent'
 import { Route, Routes } from 'react-router-dom'
 import BookProfile from './components/BookProfile'
+import { debounce } from 'throttle-debounce'
+import factoryWithThrowingShims from 'prop-types/factoryWithThrowingShims'
 // import BookProfileCC from './components/BookProfileCC'
+// import * as funcs from './HelpFunc'
+//import Moment from 'react-moment'
 
 class BooksApp extends React.Component {
   state = {
+    cTime:Date.now(), 
     searchResults:[], 
     BookShelvs: [
         {id:'currentlyReading', name:'Currently Reading' },
@@ -21,6 +26,7 @@ class BooksApp extends React.Component {
     BooksAPI.getAll()
       .then((books)=>this.setState({books}))
   }
+  componentDidUpdate = () =>{ }
   
   moveTo = (book, sh) => {
 
@@ -43,17 +49,25 @@ class BooksApp extends React.Component {
   search = (searchTerm) =>{
     // console.log(searchTerm)
     if (searchTerm.length > 0) {
-      BooksAPI.search(searchTerm)
-        .then(searchResults =>{
-          // console.log(`searchResults(${searchResults.length})`, searchResults)
-          if(searchResults.length){
-            this.setState({searchResults})          
-          } else {
-            this.setState({searchResults:[]})
-          }
-            return searchResults
-        })
+      // funcs.throttle(3000,
+
+      // )
+      debounce(5000, this.searchB(searchTerm))
     }
+  }
+  searchB = (searchTerm) => {
+    BooksAPI.search(searchTerm)
+    .then(searchResults =>{
+      console.log(`debounce fetch apply ${searchTerm}`)
+
+      // console.log(`searchResults(${searchResults.length})`, searchResults)
+      if(searchResults.length){
+        this.setState({searchResults})          
+      } else {
+        this.setState({searchResults:[]})
+      }
+        return searchResults
+    })
   }
 
   resetSearch = () => {
